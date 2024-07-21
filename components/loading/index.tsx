b78634f35ac5d58e253e892d411c5baf6256df8c
fedexin40 <federico.cruz.lozano@hotmail.com>
2024-07-21 13:55:10 -0600

    Improve the spinning view

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,12 +1,36 @@
+import Image from 'next/image';
+
 export default function Loading() {
   return (
-    <div
-      className="text-surface inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-      role="status"
-    >
-      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-        Loading...
-      </span>
+    <div className="relative">
+      <div
+        className="text-surface absolute inline-block h-14 w-14 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+      <div className="absolute">
+        <div className="relative left-1 top-1 hidden h-[47px] w-[47px] dark:block">
+          <Image
+            className="object-contain"
+            src={'/logoBlanco.png'}
+            alt=""
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+        <div className="relative left-1 top-1 block h-[47px] w-[47px] dark:hidden">
+          <Image
+            className="object-contain"
+            src={'/logoNegro.png'}
+            alt=""
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+      </div>
     </div>
   );
 }
