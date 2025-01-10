bf97607f8dc6c1e2001f22ef7c0a505a456f319b
fedexin40 <federico.cruz.lozano@hotmail.com>
2025-01-09 23:12:53 -0600

    Fix orders and h1 tags

--- app/page/[slug]/page.tsx
+++ app/page/[slug]/page.tsx
@@ -7,6 +7,8 @@ type block = {
   data: {
     text?: string;
     items?: string[];
+    style?: string;
+    level?: number;
   };
 };
 
@@ -18,19 +20,45 @@ type pageType = {
 
 function Item({ block }: { block: block }) {
   if (block.type == 'header') {
-    return (
-      <div className="py-3 pt-14 text-base font-semibold capitalize">
-        {ReactHtmlParser(block.data.text)}
-      </div>
-    );
+    if (block.data.level == 1)
+      return (
+        <div className="py-3 pt-14 text-base font-semibold capitalize">
+          <h1>{ReactHtmlParser(block.data.text)}</h1>
+        </div>
+      );
+    else if (block.data.level == 2)
+      return (
+        <div className="py-3 pt-14 text-base font-semibold capitalize">
+          <h2>{ReactHtmlParser(block.data.text)}</h2>
+        </div>
+      );
+    else if (block.data.level == 3)
+      return (
+        <div className="py-3 pt-14 text-base font-semibold capitalize">
+          <h3>{ReactHtmlParser(block.data.text)}</h3>
+        </div>
+      );
   } else if (block.type == 'paragraph') {
     return <div className="py-2 text-justify">{ReactHtmlParser(block.data.text)}</div>;
   } else if (block.type == 'list') {
     if (block.data.text) {
       return <div className="text-justify">{ReactHtmlParser(block.data.text)}</div>;
     } else if (block.data.items) {
+      if (block.data.style == 'ordered') {
+        return (
+          <ol className="list-inside list-decimal">
+            {block.data.items.map((item) => {
+              return (
+                <li className="pb-4 text-left md:text-justify" key={item}>
+                  {ReactHtmlParser(item)}
+                </li>
+              );
+            })}
+          </ol>
+        );
+      }
       return (
-        <div>
+        <ul className="list-disc">
           {block.data.items.map((item) => {
             return (
               <li className="pb-4 text-left md:text-justify" key={item}>
@@ -38,7 +66,7 @@ function Item({ block }: { block: block }) {
               </li>
             );
           })}
-        </div>
+        </ul>
       );
     }
   }
