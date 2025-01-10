081af35dba503dfc807f55183a9dfc74cf4289f6
fedexin40 <federico.cruz.lozano@hotmail.com>
2025-01-10 17:14:48 -0600

    Fix header styless

--- components/htmlParser/page.tsx
+++ components/htmlParser/page.tsx
@@ -22,19 +22,19 @@ function Item({ block }: { block: block }) {
     if (block.data.level == 1)
       return (
         <div className="py-3 pt-14 text-base font-semibold capitalize">
-          <h1>{ReactHtmlParser(block.data.text)}</h1>
+          <h1 className="text-left">{ReactHtmlParser(block.data.text)}</h1>
         </div>
       );
     else if (block.data.level == 2)
       return (
         <div className="py-3 pt-14 text-base font-semibold capitalize">
-          <h2>{ReactHtmlParser(block.data.text)}</h2>
+          <h2 className="text-left">{ReactHtmlParser(block.data.text)}</h2>
         </div>
       );
     else if (block.data.level == 3)
       return (
         <div className="py-3 pt-14 text-base font-semibold capitalize">
-          <h3>{ReactHtmlParser(block.data.text)}</h3>
+          <h3 className="text-left">{ReactHtmlParser(block.data.text)}</h3>
         </div>
       );
   } else if (block.type == 'paragraph') {
