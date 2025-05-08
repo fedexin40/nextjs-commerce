143fc91aeca5954c1859f654ee8fff51930338d5
fedexin40 <federico.cruz.lozano@hotmail.com>
2025-05-07 18:38:04 -0600

    Fix newslatter

--- components/brevo/page.tsx
+++ components/brevo/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 
 export default function Brevo() {
   return (
-    <div className="h-[630px] lg:h-[450px]">
+    <div className="h-[530px] lg:h-[380px]">
       <Suspense>
         <iframe src="/brevo.html" width="100%" height="100%"></iframe>
       </Suspense>
