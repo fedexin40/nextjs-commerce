66a06e1ecf3510a712dda350f65573286e04ce17
fedexin40 <federico.cruz.lozano@hotmail.com>
2025-07-03 14:58:45 -0600

    Fix middlewarew

--- middleware.ts
+++ middleware.ts
@@ -15,7 +15,11 @@ export async function middleware(request: NextRequest) {
   }
 
   // Add a new header x-current-path which passes the path to downstream components
-  const headers = new Headers(request.headers);
-  headers.set('x-current-path', request.nextUrl.pathname);
-  return NextResponse.next({ headers });
+  const response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+  response.headers.set('x-current-path', request.nextUrl.pathname);
+  return response;
 }
