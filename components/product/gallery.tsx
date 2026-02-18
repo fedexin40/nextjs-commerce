690447de3a22e01ea317ef674b85ee9ec4b9a998
Federico Cruz <federico.cruz.lozano@hotmail.com>
2026-02-18 16:34:20 -0600

    Update carousel with dots so customer knows there are more items

--- components/product/gallery.tsx
+++ components/product/gallery.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { PageItem } from 'components/htmlParser/page';
-import MyImage from 'components/image';
 import Image from 'next/image';
 import { useState } from 'react';
+import { ImagesMobile } from './carousel';
 
 export function Gallery({
   images,
@@ -16,17 +16,7 @@ export function Gallery({
   return (
     <>
       <div className="py-10 md:hidden">
-        <div>
-          <ul className="flex w-full gap-4 overflow-x-auto overflow-y-hidden pt-1">
-            {images.map((image) => (
-              <li key={image.src} className="aspect-square w-[250px] flex-none">
-                <div className="relative h-64 w-64">
-                  <MyImage src={image.src} />
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <ImagesMobile images={images} />
       </div>
       <div className="hidden md:grid md:h-[550px] md:grid-rows-4 lg:grid-cols-4 lg:grid-rows-1">
         <div className="hidden gap-y-3 pr-10 lg:flex lg:flex-col">
@@ -54,7 +44,7 @@ export function Gallery({
             sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
           />
         </div>
-        <div className=" mt-3 hidden flex-row gap-x-3 md:flex md:w-[410px] lg:hidden lg:w-[500px]">
+        <div className="mt-3 hidden flex-row gap-x-3 md:flex md:w-[410px] lg:hidden lg:w-[500px]">
           {images.map((image: { src: string; altText: string }, index) => (
             <div className="relative h-20 w-20" key={image.src}>
               <Image
