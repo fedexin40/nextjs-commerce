c305196f8996ff95c5eb3cc629e7b423bf4e2cdb
Federico Cruz <federico.cruz.lozano@hotmail.com>
2026-02-18 16:41:59 -0600

    Fix swipable on mobile

--- components/product/carousel.tsx
+++ components/product/carousel.tsx
@@ -24,7 +24,7 @@ export function ImagesMobile({ images }: { images: { src: string; altText: strin
   return (
     <>
       <Carousel
-        swipeable={false}
+        swipeable={true}
         draggable={false}
         showDots={true}
         responsive={responsive}
