d8a1f79f6b8c69f0930cd3234d781d4cffc4a37c
Federico Cruz <federico.cruz.lozano@hotmail.com>
2026-02-28 19:50:23 -0600

    Improve SEO

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,20 @@
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000';
+import type { MetadataRoute } from 'next';
 
-export default function robots() {
+const siteUrl = (process.env.NEXT_PUBLIC_VERCEL_URL ?? 'https://proyecto705.com.mx').replace(
+  /\/$/,
+  '',
+);
+
+export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
+        allow: '/',
+        disallow: ['/checkout-address', '/checkout-carrier', '/checkout-payment', '/user'],
       },
     ],
-    sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl,
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
   };
 }
