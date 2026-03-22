2a3cef7f38cf7a7036b381a9c3eaef44178aa257
Federico Cruz <federico.cruz.lozano@hotmail.com>
2026-03-22 13:10:17 -0600

    Improve performance

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -35,6 +35,7 @@ export default async function HomePage() {
           alt="Banner Proyecto 705"
           fill
           preload={true}
+          loading="eager"
           sizes="100vw"
           className="object-cover object-center"
         />
